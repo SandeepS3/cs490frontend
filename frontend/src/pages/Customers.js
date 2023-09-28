dacf43f2a821e07158056d8f737b62eda5fe2d23
Sandeep Singh <singh.deep8302@gmail.com>
2023-09-28 17:48:17 -0400

    homepage almost done

--- frontend/src/pages/Customers.js
+++ frontend/src/pages/Customers.js
@@ -15,10 +15,65 @@ function Customers() {
     }, [])
 
 
+    const [inputName, setNameValue] = useState('');
+    const handleNameInputChange = (event) => {
+        setNameValue(event.target.value);
+    };
+
+    const [inputID, setID] = useState('');
+    const handleIDInputChange = (event) => {
+        setID(event.target.value);
+    };
+    
+    function handleClick(isName) {
+        if (isName) {
+            axios.post('http://localhost:8384/customername', {name: inputName})
+            .then((response) => {
+                setCustomerData(response.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+        }
+        else {
+            axios.post('http://localhost:8384/customerid', {id: inputID})
+            .then((response) => {
+                setCustomerData(response.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+        }
+
+    }
+
+    const [customerDetailData, setCustomerDetailData] = useState([]);
+    const GetCustomerDetail = async (customer) => {
+        setCustomerDetailData(customer);
+        console.log(customerDetailData)
+    }
+
     return (
         <div>
             <NavBar />
             <h1>Customers Page</h1>
+            <label htmlFor="myNameInput">Enter Name:</label>
+            <input
+                type="text"
+                id="myInput"
+                value={inputName}
+                onChange={handleNameInputChange}
+            />
+            <button onClick={() => handleClick(true)}>Search</button>
+
+            <label htmlFor="myIDInput">Enter ID:</label>
+            <input
+                type="text"
+                id="myIDInput"
+                value={inputID}
+                onChange={handleIDInputChange}
+            />
+            <button onClick={() => handleClick(false)}>Search</button>
             <div>
             <h2>Customers</h2>
             <ul>
@@ -26,8 +81,9 @@ function Customers() {
                     <li 
                     className="list-group-item"
                     key={index}
+                    onClick={() => GetCustomerDetail(customer)}
                     >
-                        {customer.name}
+                        {customer.first_name} {customer.last_name}
                     </li>
                 ))}
             </ul>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,10 +3,9 @@ import axios from 'axios';
 import NavBar from '../components/NavBar'
 function HomePage() {
     const [filmData, setFilmData] = useState([]);
-    const [isFilmLoading, setIsFilmLoading] = useState(true);
     const [actorData, setActorData] = useState([]);
-    const [isActorLoading, setIsActorLoading] = useState(true);
     const [filmDetailData, setFilmDetailData] = useState([]);
+    const [actorDetailData, setActorDetailData] = useState([]);
     
     useEffect(() => {
         axios.get('http://localhost:8384/top5films')
@@ -29,6 +28,17 @@ function HomePage() {
     const GetFilmDetail = async (film) => {
         setFilmDetailData(film);
         console.log(filmDetailData)
+    }
+
+    const getActorDetails = (actor) => {
+        axios.post('http://localhost:8384/actordetail', {id: actor.actor_id})
+        .then((response) => {
+            setActorDetailData(response.data)
+            console.log(response.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        });
     }
 
   return (
@@ -67,16 +77,29 @@ function HomePage() {
         <h2>Top Five Actors</h2>
         <div>
         <ul>
-          {actorData.map((actors, index) => (
+          {actorData.map((actor, index) => (
             <li 
             className="list-group-item"
             key={index}
+            onClick={() => getActorDetails(actor)}
             >
-                {actors.first_name} {actors.last_name}
+                {actor.first_name} {actor.last_name}
             </li>
           ))}
         </ul>
         </div>
+
+        <div>
+            <h1>Actor Details</h1>
+            { actorDetailData !== null && (
+                Object.entries(actorDetailData).map(([key, value]) => (
+                <p>
+                    {key}: {value}
+                </p>
+                ))
+            )}
+        </div>
+
     </div>
 
     
